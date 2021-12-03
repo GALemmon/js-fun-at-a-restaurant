06759055e1a53dd3f3db88f6ff631811810a1cd7
George Lemmon <george.lemmon@gmail.com>
2021-12-02 17:21:22 -0700

    Add warm-up.js and meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,58 @@
-function nameMenuItem() {
 
+function nameMenuItem(foodItem) {
+  return `Delicious ${foodItem}`;
+};
+
+//------------------------------------
+
+function createMenuItem(food, price, type) {
+  var menuItem = {
+    name: food,
+    price: price,
+    type: type
+  };
+  return menuItem;
+};
+
+//-------------------------------------
+
+function addIngredients(item, ingredients){
+  if (!ingredients.includes(item)) {
+  ingredients.push(item);
+  }
+  return ingredients;
+};
+
+//---------------------------------------
+
+function formatPrice(x) {
+  return '$' + x;
+};
+
+//------------------------------------------
+
+function decreasePrice(ip) {
+  return ip - (ip * .1);
+};
+
+//--------------------------------------------
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return recipe;
 }
 
+//--------------------------------------------
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
